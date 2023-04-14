15d697308d8752756bddd271ab78e5550db67ed6
Subham Saha <subham@Subhams-MacBook-Pro.local>
2023-04-15 00:02:56 +0530

    added sitemap and meta tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -89,6 +89,7 @@ const Home = () => {
   <meta name="keywords" content="HTML, CSS, JavaScript"/>
   <meta name="author" content="John Doe"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+  <meta name="google-site-verification" content="37XMjtArBSHzAzW2I1PjFiia8kIgPjk0rqvwL_4kKSM" />
     </Head>
 
 
